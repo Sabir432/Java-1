22eb74798fb690751d3be96ae7608bda963ef6c0
Sabir <123@gmail.com>
2024-08-02 19:09:10 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+var myWorld =prompt('Введите ваше имя')
+var myWorld2 =+prompt('Введите ваш возрост')
+
+const example1 = +prompt('Решите пример 8 + 9 = ?')
+const example2 = +prompt('Решите пример 8 - 9 = ?')
+const example3 = +prompt('Решите пример 8 * 9 = ?')
+const example4 = +prompt('Решите пример 8 / 9 = ?')
+
+
+console.log(`Пример 4 + 7, вы ввелли ${example1}, ответ: 11`);
+console.log(`Пример 4 + 7, вы ввелли ${example2}, ответ: -1`);
+console.log(`Пример 4 + 7, вы ввелли ${example3}, ответ: 72`);
+console.log(`Пример 4 + 7, вы ввелли ${example4}, ответ: 11`);
+
